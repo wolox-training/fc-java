03b802c68ca0f5ab9348737f08af9bb6a3f0af59
facucompe <facu.compe@gmail.com>
2018-12-13 18:49:20 -0300

    Add endpoints in booksController

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -3,6 +3,16 @@ package wolox.training.repositories;
 import org.springframework.data.repository.Repository;
 import wolox.training.models.Book;
 
+import java.util.List;
+
 public interface BookRepository extends Repository<Book, Long> {
     Book findByAuthor(String author);
+
+    Book findById(Long id);
+
+    List<Book> findAll();
+
+    Book save(Book book);
+
+    void deleteById(Long id);
 }
