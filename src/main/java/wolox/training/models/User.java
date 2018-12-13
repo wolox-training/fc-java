d57dc68bb18cbfe453c481636ceb13f4ddfc28c0
facucompe <facu.compe@gmail.com>
2018-12-13 18:58:07 -0300

    Create User model

---
+++ src/main/java/wolox/training/models/User.java
@@ -0,0 +1,52 @@
+package wolox.training.models;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.time.LocalDate;
+import java.util.Collection;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate birthdate;
+
+    @Column(nullable = false)
+    private Collection<Book> books;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public Collection<Book> getBooks() {
+        return books;
+    }
+
+    public User() {}
+}
