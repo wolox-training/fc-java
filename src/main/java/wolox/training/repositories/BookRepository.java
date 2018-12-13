22afef920521ffbad5233344ff034842d6427c1f
facucompe <facu.compe@gmail.com>
2018-12-13 16:40:52 -0300

    Change the repository type

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,8 +1,8 @@
 package wolox.training.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 import wolox.training.models.Book;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends Repository<Book, Long> {
     Book findByAuthor(String author);
 }
