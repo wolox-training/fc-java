170703046969fc651b29cc39824c80522c47332b
facucompe <facu.compe@gmail.com>
2018-12-17 12:47:52 -0300

    Add methods to book controller

---
+++ src/main/java/wolox/training/exceptions/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package wolox.training.exceptions;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
