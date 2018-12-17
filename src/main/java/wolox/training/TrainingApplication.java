681b5243425a277ee4f1fe642a63b86ec8af9bf5
facucompe <facu.compe@gmail.com>
2018-12-17 16:42:51 -0300

    Books repository (#4)
    * Kick-off (#1)
    
    * kick-off
    
    * Add missing final lines
    
    * Add Book reposistory
    
    * Change the repository type

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import wolox.training.models.Book;
 
 @EnableJpaRepositories("org.baeldung.persistence.repo")
 @EntityScan("org.baeldung.persistence.model")
