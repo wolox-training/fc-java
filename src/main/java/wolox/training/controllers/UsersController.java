7d24355c1081af6f7f07e89c6376fe1fbec0ebe9
facucompe <facu.compe@gmail.com>
2019-01-02 10:00:18 -0300

    Add endpoint 'me' that returns logged user information

--- src/main/java/wolox/training/controllers/UsersController.java
+++ src/main/java/wolox/training/controllers/UsersController.java
@@ -3,6 +3,8 @@ package wolox.training.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import wolox.training.exceptions.BookAlreadyOwnedException;
 import wolox.training.exceptions.BookNotFoundException;
@@ -73,5 +75,12 @@ public class UsersController {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new BookNotFoundException(userId));
         user.removeBook(book);
+    }
+
+    @GetMapping("/me")
+    public User me() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+        return userRepository.findByName(currentUserName);
     }
 }
